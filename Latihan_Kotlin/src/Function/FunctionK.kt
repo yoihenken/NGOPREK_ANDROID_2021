cc76fd9c91464bf5f716f45c44efcae11d35a135
yoihenken <bagusbs226@gmail.com>
2021-10-31 10:34:40 +0700

    Create FunctionK.kt

---
+++ Latihan_Kotlin/src/Function/FunctionK.kt
@@ -0,0 +1,33 @@
+package Function
+
+fun main(){
+    val user = functionUser("Bagus", 20)
+
+    println(user)
+    procedureUser("Bayu")
+
+    val userQuick = functionQuick("Lisa", 17)
+    println(userQuick)
+
+}
+
+// Method : 2 jenis
+/*
+*  - Fungsi (Function) = memiliki tipe data dan mengembalikan nilai
+*  - Prosedur (Procedure) = jalan ampe selesai tidak mengembalikan nilai
+* */
+
+//Prosedur
+fun procedureUser(name: String){
+    println("Kimi no nawa $name")
+}
+
+//Fungsi
+fun functionUser(name : String, age : Int) : String{ //fun _namafungsi_ (_parameter) : _tipedataDariFungsi
+    val output = "Kimi no nawa $name desu, $age sai desu"
+    return output
+}
+
+//Fungsi penulisan cepat
+fun functionQuick(name: String, age: Int) = "Your name is $name, $age years old" 
+
